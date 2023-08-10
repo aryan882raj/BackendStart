b034ba06a5b3a9ce6909fb513c9d51e2bdcf856d
aryan882raj <aryan882raj@gmail.com>
2023-08-10 11:23:56 +0530

    url/home-/about-/node

--- serverapp.js
+++ serverapp.js
@@ -6,11 +6,66 @@ const http = require('http');  // path or module
 // or--------------------------
 // http.createServer(function(res,req){})
 
-// or-------------------------
-const server = http.createServer((res,req)=>{
-    console.log("Aryan",req);
+
+
+
+
+// or-------------------------  request
+// const server = http.createServer((res,req)=>{
+//     console.log("Aryan",req);
+//     // process.exit()                              // to stop web page  noone can acess now
+// });
+// server.listen(4000);
+
+// ------------------------------------ response
+// const server = http.createServer((req,res)=>{
+//     console.log(req.url , req.method ,req.headers);
+//     // process.exit()                              // to stop web page  noone can acess now
+
+//     res.setHeader('Content-Type','text/html');
+//     res.write('<html>')    // write html page
+//     res.write('<head><title>My First Page</title></head>')
+//     res.write('<body><h1>Hello</h1></body>');
+//     res.write('</html>')
+//     res.end()              //response endes 
+// });
+// server.listen(3000);
+
+
+
+
+
+
+
+
+
+
+const server = http.createServer((req,res)=>{
+    console.log(req.url , req.method ,req.headers);
+    // process.exit()                              // to stop web page  noone can acess now
+    if(req.url==='/home'){
+        res.setHeader('Content-Type','text/html');
+        res.write('<html>')    // write html page
+        res.write('<head><title>My First Page</title></head>')
+        res.write('<body><h1>Welcome home</h1></body>');
+        res.write('</html>')
+        res.end()              //response endes 
+    }
+    if(req.url==='/about'){
+        res.setHeader('Content-Type','text/html');
+        res.write('<html>')    // write html page
+        res.write('<head><title>My First Page</title></head>')
+        res.write('<body><h1>Welcome to about us page</h1></body>');
+        res.write('</html>')
+        res.end()              //response endes 
+    }
+    if(req.url==='/node'){
+        res.setHeader('Content-Type','text/html');
+        res.write('<html>')    // write html page
+        // res.write('<head><title>My First Page</title></head>')
+        res.write('<body><h1>Welcome to my Node Project</h1></body>');
+        res.write('</html>')
+        res.end()              //response endes 
+    }
 });
-
-server.listen(4000);
-
-
+server.listen(3000);
