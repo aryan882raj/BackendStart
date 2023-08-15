473e164829cc6f194fa29d71858e3b8d2b5b38ed
aryan882raj <aryan882raj@gmail.com>
2023-08-15 14:12:47 +0530

    app.listen EXPRESS

--- app.js
+++ app.js
@@ -1,6 +1,22 @@
-const http = require('http'); 
-const routes = require('./app_route');            // import
-
-const server = http.createServer(routes)          // use is instead (req,res)=>{}
+// const http = require('http'); 
+// const routes = require('./app_route');            // import
+// const server = http.createServer(routes)          // use is instead (req,res)=>{}
                                                   // or (routes.handler) // (routes.somtext)
-server.listen(3000);
+const express = require('express');
+const app = express();
+
+app.use((req , res , next)=>{
+    console.log("In the middleware");
+    next();                                           // to jump nextmiddleware
+})
+app.use((req , res , next)=>{
+    console.log("Another middleware");
+    // res.send('<h1>Hello Express</h1>')
+    res.send( { "name": 123 })
+})
+                  
+
+
+app.listen(3000);
+// const server = http.createServer(app)
+// server.listen(3000);
