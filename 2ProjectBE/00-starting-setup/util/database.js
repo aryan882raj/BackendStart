5c11cb837068e431db33cb8aaf1587d672eebe24
aryan882raj <aryan882raj@gmail.com>
2023-09-24 12:49:52 +0530

    with Sequelizer

--- 2ProjectBE/00-starting-setup/util/database.js
+++ 2ProjectBE/00-starting-setup/util/database.js
@@ -1,11 +1,19 @@
-const mysql = require('mysql2');
+// const mysql = require('mysql2');
 
-const pool = mysql.createPool({
-    host:'localhost',
-    user:'root',
-    database:'node-complete',
-    password:'aryan'
+// const pool = mysql.createPool({
+//     host:'localhost',
+//     user:'root',
+//     database:'node-complete',
+//     password:'aryan'
+// })
+
+// module.exports = pool.promise();
+
+
+const Sequelize = require('sequelize')
+const sequelize = new Sequelize('node-complete','localhost','aryan',{
+    dialect:'mysql',
+    host:'localhost'
 })
 
-
-module.exports = pool.promise();
+module.exports = sequelize;
